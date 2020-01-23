cf7452ab90a89ceea0e4502a37368bd7f92c55d7
bngams <n.boris@devidia.net>
2020-01-23 11:22:44 +0100

    project initialize

---
+++ src/main/java/fr/cesi/rila19/samplespringdemo/SampleSpringDemoRil19Application.java
@@ -0,0 +1,13 @@
+package fr.cesi.rila19.samplespringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringDemoRil19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringDemoRil19Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cesi/rila19/samplespringdemo/SampleSpringDemoRil19ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cesi.rila19.samplespringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringDemoRil19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
